1e15bb102844f476f4383fc055bffaadf9cde683
Edo Barnas <iam@edobarnas.com>
2024-05-02 15:18:33 +0200

    Add namespace to the bundle class

--- src/EcomailBundle.php
+++ src/EcomailBundle.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+namespace Tuzex\Ecomail;
+
+use Ecomail;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
